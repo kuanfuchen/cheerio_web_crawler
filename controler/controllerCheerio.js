9928de2cd3aa92751f53f4a986b16485f5b95a83
kuanfuchen <creazy_fu@hotmail.com>
2023-09-22 17:11:11 +0800

    read ptt

--- controler/controllerCheerio.js
+++ controler/controllerCheerio.js
@@ -2,7 +2,7 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const puppeteer = require('puppeteer'); 
 const handleSuccess = require('../service/handleSuccess');
-
+const handleFail = require('../service/handleFail');
 const bankExchangeRate = (req,res ,next) =>{
   try{
   const targetUrl = req.body.targetUrl;
@@ -35,25 +35,46 @@ const crawelPtt = (req, res, next)=>{
   try{
     const targetUrl = req.body.targetUrl;
     axios.get(targetUrl).then( async(response)=>{
-      let $ = cheerio.load(response.data);
-      const brower = await puppeteer.launch({ headless:false });
-      const page = await brower.newPage();
-      await page.goto(targetUrl);
-      const buttonSelector = 'body > div.bbs-screen.bbs-content.center.clear > form > div:nth-child(2) > button';  // 已滿18歲按鈕 selector
-      await page.click(buttonSelector);  // 按下按鈕
-      const content = await page.content();
-      $ = cheerio.load(content);
-      const info = [];
-      const list = $(".r-list-container .r-ent");
-      for (let i = 0; i < list.length; i++) {
-        const title = list.eq(i).find('.title a').text();
-        const author = list.eq(i).find('.meta .author').text();
-        const date = list.eq(i).find('.meta .date').text();
-        const link = list.eq(i).find('.title a').attr('href');
-        info.push({ title, author, date, link });
+        let $ = cheerio.load(response.data);
+        try{
+        // ptt驗證
+        const brower = await puppeteer.launch({ headless:false });//開啟一個瀏覽器
+        const page = await brower.newPage();//開啟一個分頁
+        await page.goto(targetUrl);//前往指定頁面
+        const buttonSelector = 'body > div.bbs-screen.bbs-content.center.clear > form > div:nth-child(2) > button';  // 已滿18歲按鈕 selector
+        await page.click(buttonSelector);  // 按下按鈕
+        await page.waitForSelector('div.r-list-sep');
+        const content = await page.content();  
+        // // await page.screenshot({path: 'google.png'});//截圖
+        
+        // // r-list-sep
+        $ = await  cheerio.load(content);
+        // 
+        const info = [];
+        const list = $(".r-list-container .r-ent");
+        const prevPage = $(".action-bar .btn-group-paging");
+        const prevPagelist = prevPage.eq(0).find('.wide');
+        for(let i = 0 ; prevPagelist.length > i ; i++){
+          const pageTitle = prevPagelist.eq(i).text();
+          const pageAttr = prevPagelist.eq(i).attr('href')
+          console.log(pageTitle, pageAttr)
+        }
+        
+        for (let i = 0 ; i < list.length; i++) {
+          // if(i===0){
+          //   console.log(list.eq(i).find('.title a').text())
+          // }
+          const title = list.eq(i).find('.title a').text();
+          const author = list.eq(i).find('.meta .author').text();
+          const date = list.eq(i).find('.meta .date').text();
+          const link = list.eq(i).find('.title a').attr('href');
+          info.push({ title, author, date, link });
+        }
+        handleSuccess(res, info)
+        // await brower.close();//關閉瀏覽器
+      }catch(err){
+        handleFail(res, '爬蟲失敗')
       }
-      handleSuccess(res, info)
-
     })
   }catch(err){
     handleFail(res, '爬蟲失敗')
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {bankExchangeRate, crawelPtt} = require('../controler/controllerCheerio');
-/* GET home page. */
 router.post('/bankExchangeRate', function(req, res, next) {
   bankExchangeRate(req, res, next)
-  // res.render('index', { title: 'Express' });
 });
 router.post('/crawelPtt', function(req, res, next) {
   crawelPtt(req, res, next)
-  // res.render('index', { title: 'Express' });
 });
-
 module.exports = router;
